f44493885f6ff1a1648f706aae81d05c760a0384
SungYeon Kim <gkgktjddus@gmail.com>
2023-08-14 09:33:24 +0900

    ㅇ

---
+++ src/main/java/org/project/proejct/ProejctApplication.java
@@ -0,0 +1,13 @@
+package org.project.proejct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProejctApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProejctApplication.class, args);
+	}
+
+}
