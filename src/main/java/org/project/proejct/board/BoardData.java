88375ce3d74f9e7aa9a1d61cead9bcedef445c4b
SungYeon Kim <gkgktjddus@gmail.com>
2023-08-14 09:44:25 +0900

    게시판 데이타 추가

---
+++ src/main/java/org/project/proejct/board/BoardData.java
@@ -0,0 +1,14 @@
+package org.project.proejct.board;
+
+import java.time.LocalDateTime;
+
+public class BoardData {
+
+    private String id;
+    private String poster;
+    private String subject;
+    private String content;
+    private LocalDateTime regDt;
+    private LocalDateTime modDt;
+
+}
---
+++ src/test/java/org/project/proejct/test/BoardServiceTest.java
@@ -0,0 +1,10 @@
+package org.project.proejct.test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BoardServiceTest {
+
+
+
+}
